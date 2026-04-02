2aa1bd05e1be808f5ea83aa25aa3bd0b70d56721
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-02 12:58:10 +0600

    Create quicksort_user_input_before_after.cpp

---
+++ year_24/quicksort_user_input_before_after.cpp
@@ -0,0 +1,44 @@
+ #include <iostream>
+using namespace std;
+
+int partition(int A[], int low, int high) {
+    int pivot = A[high];
+    int i = low - 1;
+
+    for(int j = low; j < high; j++) {
+        if(A[j] < pivot) {
+            i++;
+            swap(A[i], A[j]);
+        }
+    }
+
+    swap(A[i + 1], A[high]);
+    return i + 1;
+}
+
+void quickSort(int A[], int low, int high) {
+    if(low < high) {
+        int pi = partition(A, low, high);
+        quickSort(A, low, pi - 1);
+        quickSort(A, pi + 1, high);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int A[n];
+
+    for(int i = 0; i < n; i++)
+        cin >> A[i];
+
+    for(int i = 0; i < n; i++)
+        cout << A[i] << " ";
+
+    cout << endl;
+
+    quickSort(A, 0, n - 1);
+
+    for(int i = 0; i < n; i++)
+        cout << A[i] << " ";
+}
