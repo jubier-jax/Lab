90380abbaa6e8446c36a3f8ca0b43e8e8187b02a
Bayjid Alam Jubier <csejubier@gmail.com>
2026-04-02 12:54:46 +0600

    Create Insertion_deletion.cpp

---
+++ year_24/Insertion_deletion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[100];
+
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int val, pos;
+    cin >> val >> pos;
+
+    for(int i = n; i > pos; i--)
+        a[i] = a[i - 1];
+
+    a[pos] = val;
+    n++;
+
+    int delPos;
+    cin >> delPos;
+
+    for(int i = delPos; i < n - 1; i++)
+        a[i] = a[i + 1];
+
+    n--;
+
+    for(int i = 0; i < n; i++)
+        cout << a[i] << " ";
+}
